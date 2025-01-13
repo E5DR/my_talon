19b8f9d789abdd4d5c0e5f99b23f2fb50040f7a4
markus <markus>
2025-01-13 11:52:18 +0100

    recognize vscode if it shows up as just "Electron"
    this sometimes happens on my system

--- apps/vscode/vscode.py
+++ apps/vscode/vscode.py
@@ -1,3 +1,14 @@
+from talon import Module
+
+mod = Module()
+
+# on my system vscode sometimes shows up as just "Electron"
+mod.apps.vscode = """
+os: linux
+and app.name: Electron
+and win.title: /Code - OSS/
+"""
+
 # focus "commmunity" vscode window
 # candidates = [w for w in ui.apps(name="Code")[0].windows() if "community" in w.title]
 # candidates[0].focus()
